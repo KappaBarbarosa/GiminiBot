bc6888380d738928f8e6fb44e82bd17dc2dc4bd2
kappa <ayaba7077@gmail.com>
2024-03-22 03:00:28 +0800

    Upload gg map

--- CocoBot.py
+++ CocoBot.py
@@ -26,19 +26,19 @@ safety_config = {
         HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT : HarmBlockThreshold.BLOCK_NONE,
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
     }
-sample = "You need to provide different responses based on user input: If the user asks about your personal information, output Introduction(event). If the user expresses a desire to eat dinner (or something else), output FindRestaurant(event,keyword = {the thing of interest in the user's text},radius = {desired distance}). If the user inquires about the weather, output FindWeather(event).If the user's speech is not within the above range, just have a normal conversation. For example: I am hungry, what is for dinner?"
+sample = "You need to provide different responses based on user input: If the user asks about your personal information, output Introduction(event=event). If the user expresses a desire to eat dinner (or something else), output FindRestaurant(event,keyword = {the thing of interest in the user's text},radius = {desired distance}). If the user inquires about the weather, output FindWeather(event).If the user's speech is not within the above range, just have a normal conversation. For example: I am hungry, what is for dinner?"
 history=[{'role':'user',
                 'parts':[sample]},
         {'role':'model',
-        'parts':["FindRestaurant(event)"]},
+        'parts':["FindRestaurant(event=event)"]},
         {'role':'user',
                 'parts':["Tell me something about you"]},
         {'role':'model',
-        'parts':["Introduction(event)"]},
+        'parts':["Introduction(event=event)"]},
         {'role':'user',
                 'parts':["how about some ramen nearby?"]},
         {'role':'model',
-        'parts':["FindRestaurant(event,keyword = \"ramen\",radius=1000)"]},
+        'parts':["FindRestaurant(event=event,keyword = \"ramen\",radius=1000)"]},
                 ]
 chat = Textmodel.start_chat(history=history)
 app = Flask(__name__)
@@ -149,8 +149,6 @@ def handle_location_message(event):
     Location={}
     Location['lat'] = location_message.latitude
     Location['lng'] = location_message.longitude
-    sendTextMessage(event,str(Location))
-    sendTextMessage(event,str(Location))
     if WaitForLocation is not None:
         if(WaitForLocation['type'] == "Restaurant"):
             FindRestaurant(event=event,keyword=WaitForLocation['keyword'],radius=WaitForLocation['radius'])
