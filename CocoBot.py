f0120e6d264829fc5c39aa6ae6303ccd619f889c
kappa <ayaba7077@gmail.com>
2024-03-25 05:23:12 +0800

    updated query

--- CocoBot.py
+++ CocoBot.py
@@ -35,11 +35,11 @@ safety_config = {
         HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT : HarmBlockThreshold.BLOCK_NONE,
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
     }
-sample = "You need to provide different responses based on user input: If the user asks about your information or who are you, output Introduction(event=event). If the user expresses a desire to eat something), output FindRestaurant(event,keyword = {the thing of interest in the user's text},radius = {desired distance}). If the user inquires about the weather, output FindWeather(event=event,query={user's query}). If the user want to change the location, output AskForUserLocation(event).If the user's speech is not within the above range, just have a normal conversation. For example: I am hungry, what is for dinner?"
+sample = "You need to provide different responses based on user input: If the user asks about your information or who are you, output Introduction(event=event). If the user expresses a desire to eat something), output FindRestaurant(event,query= {user's text}, keyword = {the thing of interest in the user's text},radius = {desired distance}). If the user inquires about the weather, output FindWeather(event=event,query={user's query}). If the user want to change the location, output AskForUserLocation(event).If the user's speech is not within the above range, just have a normal conversation. For example: I am hungry, what is for dinner?"
 history=[{'role':'user',
                 'parts':[sample]},
         {'role':'model',
-        'parts':["FindRestaurant(event)"]},
+        'parts':["FindRestaurant(event,query= \"I am hungry, what is for dinner?\")"]},
         {'role':'user',
                 'parts':["Tell me something about you"]},
         {'role':'model',
@@ -47,7 +47,7 @@ history=[{'role':'user',
         {'role':'user',
                 'parts':["how about some ramen nearby?"]},
         {'role':'model',
-        'parts':["FindRestaurant(event=event,keyword = \"ramen\",radius=1000)"]},
+        'parts':["FindRestaurant(event=event, query = \"how about some ramen nearby?\"), keyword = \"ramen\",radius=1000)"]},
         {'role':'user',
                 'parts':["Will today be hot?"]},
         {'role':'model',
@@ -99,11 +99,11 @@ def Introduction(event,**kwargs):
 def AskForUserLocation(event):
     sendTextMessage(event,"請告訴我你的位置!")
     
-def FindRestaurant(event=None,keyword="Restaurant",radius=1000):
+def FindRestaurant(event=None,query="",keyword="Restaurant",radius=1000):
     global WaitForLocation
     if Location is None:
         AskForUserLocation(event)
-        WaitForLocation = {"type":"Restaurant","keyword":keyword,"radius":radius}
+        WaitForLocation = {"type":"Restaurant","query":query,"keyword":keyword,"radius":radius}
         return "sucess"
     else:
         places_result = gmaps.places_nearby(Location, keyword=keyword,type='restaurant', radius=radius,language='zh-TW')['results']
@@ -199,7 +199,7 @@ def handle_location_message(event):
     Location['lng'] = location_message.longitude
     if WaitForLocation is not None:
         if WaitForLocation['type'] == "Restaurant":
-            FindRestaurant(event,keyword=WaitForLocation['keyword'],radius=WaitForLocation['radius'])
+            FindRestaurant(event,query=WaitForLocation['query'],keyword=WaitForLocation['keyword'],radius=WaitForLocation['radius'])
         elif WaitForLocation['type'] == "Weather":
             FindWeather(event,query=WaitForLocation['query'])
     
