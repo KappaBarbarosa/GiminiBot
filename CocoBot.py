1c01041fe6b44723d428d5d9d1f52d502aff2bf2
kappa <ayaba7077@gmail.com>
2024-03-25 05:07:22 +0800

    add weather

--- CocoBot.py
+++ CocoBot.py
@@ -118,7 +118,7 @@ def FindRestaurant(event=None,keyword="Restaurant",radius=1000):
     Update_Chat([sample,response.text])
     return "sucess"
 
-def FindWeather(event,**kwargs):
+def FindWeather(event):
     global WaitForLocation
     if Location is None:
         AskForUserLocation(event)
@@ -169,12 +169,12 @@ def handle_text_message(event):
         Update_Chat([f'I send an image to you and ask {msg}',response.text])
     else:
         response = chat.send_message(msg,safety_settings=safety_config)
-        try:
-            result = eval(response.text)
-            if result != "sucess":
-                sendTextMessage(event,result)
-        except:
-            sendTextMessage(event,response.text)
+        # try:
+        result = eval(response.text)
+        if result != "sucess":
+            sendTextMessage(event,result)
+        # except:
+        #     sendTextMessage(event,response.text)
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
