269fd8d6ab322a27d84e129fa2e7a8f1dac04aa5
kappa <ayaba7077@gmail.com>
2024-03-25 13:48:04 +0800

    add sticker

--- CocoBot.py
+++ CocoBot.py
@@ -181,7 +181,8 @@ def handle_text_message(event):
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
-    sendTextMessage(event,[event.message.stickerId,event.message.stickerId,event.message.keyword])
+    msg = event.message
+    sendTextMessage(event,[msg.sticker_id,msg.package_id,msg.sticker_resource_type,msg.keywords,msg.text])
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
